e6af93b228641e5ce8ee764d97b6e20bb80504cf
Robin Eklind <rnd0x00@gmail.com>
2019-12-30 02:30:31 +0100

    Remove old and unused llvm directory.
    As part of PR #24, Geode switched to using an unmodified version of
    llir/llvm, as such the old copy of llvm is no longer needed.

--- llvm/ir/metadata/metadata_test.go
+++
@@ -1,9 +0,0 @@
-package metadata_test
-
-import "github.com/geode-lang/geode/llvm/ir/metadata"
-
-// Validate that the relevant types satisfy the metadata.Node interface.
-var (
-	_ metadata.Node = &metadata.Metadata{}
-	_ metadata.Node = &metadata.String{}
-)
